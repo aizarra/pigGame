13fdf76d77ac4b1c63c0cb56b142d8cf9d632846
Andres <aizarra@yahoo.com>
2023-02-23 09:48:10 +0100

    added instructions

--- app.js
+++ app.js
@@ -92,8 +92,8 @@ function init() {
   document.getElementById('score--1').textContent = '0';
   document.getElementById('current--0').textContent = '0';
   document.getElementById('current--1').textContent = '0';
-  document.getElementById('name--0').textContent = 'Player 1';
-  document.getElementById('name--1').textContent = 'Player 2';
+  document.getElementById('name--0').textContent = 'Jugador 1';
+  document.getElementById('name--1').textContent = 'Jugador 2';
   document.querySelector('.player--0').classList.remove('player--winner');
   document.querySelector('.player--1').classList.remove('player--winner');
   document.querySelector('.player--0').classList.add('active');
